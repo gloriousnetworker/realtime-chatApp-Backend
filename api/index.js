a4dce0c758cd08e6435f89678b8222b1d68abdf2
gloriousnetworker <iniubongudofot@gmail.com>
2024-09-18 06:54:35 +0100

    fix CORS issues

--- api/index.js
+++ api/index.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://ydkm-chatapp.vercel.app'
+}));
 app.use(express.json());
 
 // Log environment variables for debugging
